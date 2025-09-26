411a9a012fd09f23991f9c6aab83c4ab85b9b98d
Fred Herbert <fredherbert@macbookair.home>
2025-09-26 16:07:10 +0100

    Update repository name to disco-csv-spotify

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SPOTIFY_CONFIG = {
     CLIENT_ID: 'dcc66d5be9844ed19218034b51392580',
-    REDIRECT_URI: 'http://localhost:3000/callback.html',
+    REDIRECT_URI: 'https://YOUR_USERNAME.github.io/disco-csv-spotify/callback.html',
     SCOPES: [
         'playlist-modify-public',
         'playlist-modify-private',

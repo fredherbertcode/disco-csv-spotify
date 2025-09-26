40f01981ecc1f98943dcd84ec1eb0dac3203b065
Fred Herbert <fredherbert@macbookair.home>
2025-09-26 16:13:21 +0100

    Fix authentication: redirect to index.html and enable implicit grant

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SPOTIFY_CONFIG = {
     CLIENT_ID: 'dcc66d5be9844ed19218034b51392580',
-    REDIRECT_URI: 'https://fredherbertcode.github.io/disco-csv-spotify/callback.html',
+    REDIRECT_URI: 'https://fredherbertcode.github.io/disco-csv-spotify/index.html',
     SCOPES: [
         'playlist-modify-public',
         'playlist-modify-private',
